af2d2a0f61a7d3083ecacab6071b6bbf1f743a63
akal <akalnakarmi1@gmail.com>
2024-09-06 17:21:37 +0545

    models admin register

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Bid)
+admin.site.register(models.Cart)
+admin.site.register(models.Item)
+admin.site.register(models.Listing)
+admin.site.register(models.Wishlist)
