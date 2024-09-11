d10e212d3780701c500f494bb1f579d5c0397bf4
akal <akalnakarmi1@gmail.com>
2024-09-11 07:46:55 +0545

    better cart
    added brown color changed class name to itemList and better classing in cart

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ module.exports = {
       colors: {
         primary: '#1DA1F2',  // Add a custom color
         secondary: '#14171A',
+				brown: {
+					DEFAULT : 'rgb(223,190,125)',
+					100: 'rgba(223,190,125,.1)',
+					200: 'rgba(223,190,125,.2)',
+					300: 'rgba(223,190,125,.3)',
+					400: 'rgba(223,190,125,.4)',
+					500: 'rgba(223,190,125,.5)',
+					600: 'rgba(223,190,125,.6)',
+					700: 'rgba(223,190,125,.7)',
+					800: 'rgba(223,190,125,.8)',
+					900: 'rgba(223,190,125,.9)'
+				}
       },
       spacing: {
         '72': '18rem',  // Add a custom spacing value
