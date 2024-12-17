ba0826fc70b0a0a288a9b1ceafbb5efb3a65848b
akal <akalnakarmi1@gmail.com>
2024-12-17 20:06:24 +0545

    changed to postgress and added store coin data

--- finityStore/settings.py
+++ finityStore/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'finityStore.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "finityStore",
+        "USER": "postgres",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -132,3 +136,10 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+AUTH_USER_MODEL = 'basic.CustomUser' 
+
+
+STORE_COIN_URL = 'http://localhost:8080'
+STORE_COIN_EMAIL = 'akalnakarmi1@gmail.com'
+STORE_COIN_PASSWORD = '123'
